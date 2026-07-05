5c666196894e19fc9714cff4c0227a6c74785414
mtsnowden <mtsnowden@gmail.com>
2026-07-05 17:26:04 -0500

    implement the onboarding screen

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,3 +1,4 @@
+import { Link } from "expo-router";
 import { Text, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
@@ -8,6 +9,9 @@ export default function Index() {
     <SafeAreaView style={{ flex: 1, backgroundColor: colors.neutral.background }}>
       <View className="flex-1 items-center justify-center bg-background">
         <Text className="h1 mt-90 text-center color-lingua-purple">lingua</Text>
+        <Link href="/onboarding" className="body-lg mt-6 text-lingua-purple">
+          Go to onboarding
+        </Link>
       </View>
     </SafeAreaView>
   );
